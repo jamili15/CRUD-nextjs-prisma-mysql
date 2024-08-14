5b517bb6c956fe924a0041f457736ffd8cafbc07
James Kevin Jamili <jameskevin.ramesessystems@gmail.com>
2024-08-14 09:35:23 +0800

    updated

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,18 +5,18 @@ import Todo from "@/components/Todo";
 import prisma from "@/lib/db";
 
 const Home = async () => {
-  // const data = await prisma.todo.findMany({
-  //   select: {
-  //     title: true,
-  //     id: true,
-  //     isCompleted: false,
-  //   },
-  //   orderBy: {
-  //     createdAt: "desc",
-  //   },
-  // });
+  const data = await prisma.todo.findMany({
+    select: {
+      title: true,
+      id: true,
+      isCompleted: false,
+    },
+    orderBy: {
+      createdAt: "desc",
+    },
+  });
 
-  // console.log("DATA", data);
+  console.log("DATA", data);
   return (
     <div className="w-screen py-20 flex justify-center flex-col items-center">
       {/* <h1 className=" text-3xl font-extrabold mb-5">
@@ -25,7 +25,7 @@ const Home = async () => {
 
       <div className="flex justify-center flex-col items-center w-[1000px] ">
         <AddTodo />
-        {/* <div className=" flex flex-col gap-5 items-center justify-center mt-10 w-full">
+        <div className=" flex flex-col gap-5 items-center justify-center mt-10 w-full">
           {data.map((todo: any) => (
             <div className="w-[75%] bg-slate-200 shadow-xl" key={todo.id}>
               <div className="card-body">
@@ -33,7 +33,7 @@ const Home = async () => {
               </div>
             </div>
           ))}
-        </div> */}
+        </div>
       </div>
     </div>
   );
